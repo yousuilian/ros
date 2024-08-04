d700aa0da67796d1e71d4356bd7ab4c9e9060a19
YSL <1726582664@qq.com>
2024-08-04 20:45:39 +0800

    [202408042046]学习urdf的joint语法

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/optobot/DEMO05_WS/src
+# Build directory: /home/optobot/DEMO05_WS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("urdf01_rviz")
---
+++ build/urdf01_rviz/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/optobot/DEMO05_WS/src/urdf01_rviz
+# Build directory: /home/optobot/DEMO05_WS/build/urdf01_rviz
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
